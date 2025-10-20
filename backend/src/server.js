191a0dd9382e42625c738496bde6b5b63cea57b4
Khushi-S-29 <khushiumalakshmi@gmail.com>
2025-10-20 22:11:15 +0530

    minor fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,12 +8,9 @@ const app = express();
 connectDB();
 const PORT = process.env.PORT || 5001;
 
-//middleware
 app.use(express.json());
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {
-  console.log("server started on port :" , PORT);
+  console.log("server started on port :", PORT);
 });
-
-// mongodb+srv://khushisu29_db_user:8jsVssEHxm6L7pOs@cluster0.mlzaihl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
