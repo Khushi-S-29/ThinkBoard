468701b37f117f7ea3011a2ff708b00892d53bb1
Khushi-S-29 <khushiumalakshmi@gmail.com>
2025-10-21 22:05:44 +0530

    fix: wait for DB connection before starting server

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,13 +6,14 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
-connectDB();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(rateLimiter)
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+connectDB().then(() => app.listen(PORT, () => {
   console.log("server started on port :", PORT);
-});
+})
+)
+
