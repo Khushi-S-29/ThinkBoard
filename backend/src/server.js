142b39090abd4cab2fc6834e338d969d4d11d64a
Khushi-S-29 <khushiumalakshmi@gmail.com>
2025-12-15 17:10:03 +0530

    fix: configure CORS and clean up UI

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
-app.use(rateLimiter)
+app.use(rateLimiter);
+
 app.use("/api/notes", notesRoutes);
 
-connectDB().then(() => app.listen(PORT, () => {
-  console.log("server started on port :", PORT);
-})
-)
-
+connectDB().then(() =>
+  app.listen(PORT, () => {
+    console.log("server started on port :", PORT);
+  })
+);
